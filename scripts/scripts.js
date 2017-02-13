74e781b8884ba472117dfe19f54195f6650b1852
Nicky Kwan <nickykwn@gmail.com>
2017-02-12 23:55:17 -0500

    multiple videos embedded

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,6 +22,30 @@ $('#contact').click(function() {
   }, 700);
 });
 
+$('#navAboutMe').click(function() {
+  $('html, body').animate({
+    scrollTop: $('#aboutMeDiv').offset().top
+  }, 700);
+});
+
+$('#navExperience').click(function() {
+  $('html, body').animate({
+    scrollTop: $('#experienceDiv').offset().top
+  }, 700);
+});
+
+$('#navProjects').click(function() {
+  $('html, body').animate({
+    scrollTop: $('#projectsDiv').offset().top
+  }, 700);
+});
+
+$('#navContacts').click(function() {
+  $('html, body').animate({
+    scrollTop: $('#contactDiv').offset().top
+  }, 700);
+});
+
 $(window).scroll(function(event) {
   console.log($(window).scrollTop());
   if($(window).scrollTop()>1000){
@@ -40,6 +64,29 @@ function onYouTubeIframeAPIReady() {
       height: 250,              // Player height (in px)
       playerVars: {
         autoplay: 1,        // Auto-play the video on load
+        controls: 1,        // Show pause/play buttons in player
+        showinfo: 1,        // Hide the video title
+        modestbranding: 1,  // Hide the Youtube Logo
+        loop: 1,            // Run the video in a loop
+        fs: 0,              // Hide the full screen button
+        cc_load_policy: 0, // Hide closed captions
+        iv_load_policy: 0,  // Hide the Video Annotations
+        autohide: 1,         // Hide video controls when playing
+        start: 055,
+        playlist : 'sokFwmFNwUY'
+      },
+      events: {
+        onReady: function(e) {
+          e.target.mute();
+          }
+      }
+    });
+    player2 = new YT.Player('snowboardVid', {
+      videoId: '6uFrPeIhbwk', // YouTube Video ID
+      width: 400,               // Player width (in px)
+      height: 250,              // Player height (in px)
+      playerVars: {
+        autoplay: 1,        // Auto-play the video on load
         controls: 0,        // Show pause/play buttons in player
         showinfo: 1,        // Hide the video title
         modestbranding: 1,  // Hide the Youtube Logo
@@ -48,12 +95,12 @@ function onYouTubeIframeAPIReady() {
         cc_load_policy: 0, // Hide closed captions
         iv_load_policy: 0,  // Hide the Video Annotations
         autohide: 1,         // Hide video controls when playing
-        start: 055
+        start: 009
       },
       events: {
         onReady: function(e) {
           e.target.mute();
-          }
+        }
       }
     });
 }
